e2bef4de906e2d1359071993fa874723a7fbb003
cornelius <corneliussigei@yahoo.com>
2018-10-01 17:23:45 +0300

    Removed venv directory from repo

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!D:\MY FILES\ACADEMICS\PROGRAMMING\Python\locallibrary\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
